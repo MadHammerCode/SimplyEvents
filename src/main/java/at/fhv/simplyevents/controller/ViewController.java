7aeb6d0fb07653d016e42c9df8b5baccf97d155c
Tobi S. <148350649+Tobias755@users.noreply.github.com>
2025-12-03 05:33:27 +0100

    feat: add Event Details Page (from PDP; Product Details Page) with event-details.html; add new styles in stylesheets
    fix: fix style margins and paddings; change "eventlist" to "EventList" in ViewController.java
    
    style: reformat stylesheets

--- src/main/java/at/fhv/simplyevents/controller/ViewController.java
+++ src/main/java/at/fhv/simplyevents/controller/ViewController.java
@@ -5,27 +5,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-
+    
     @GetMapping("/dashboard")
     public String authorsView() {
-        return "Dashboard"; }
-
-
-    @GetMapping("/events")
-    public String eventList() {
-        return "eventlist"; }
-
-    @GetMapping("/login")
-    public String showLoginPage() {
-        return "login"; }
-
+        return "Dashboard";
+    }
+    
+    @GetMapping("/event-details")
+    public String showEventDetailsPage() {
+        return "event-details";
+    }
+    
     @GetMapping("/booking")
     public String showBookingPage() {
         return "booking";
     }
-
+    
     @GetMapping("/cancel-booking")
     public String showCancelBookingPage() {
         return "cancel-booking";
+    }
+    
+    @GetMapping("/events")
+    public String eventList() {
+        return "EventList";
+    }
+    
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
     }
 }
